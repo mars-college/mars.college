03a3adaa37f07f91bc5ce09a650e7c4f76bf7800
genekogan <kogan.gene@gmail.com>
2020-08-07 00:46:21 -0700

    increase gallery

---
+++ _gallery/parlor.md
@@ -0,0 +1,5 @@
+---
+layout: gallery
+title: Mars College
+target_tag: parlor
+---
--- scripts/process_images.py
+++ scripts/process_images.py
@@ -13,20 +13,22 @@ height_small = 300
 height_large = 400
 
 
-    
 data_path = '../_data/gallery.json'
 image_dir = '../images/gallery/'
 thumbs_dir = '../images/gallery/thumb/'
-lookup_path = 'gdrive_lookup.json'
+gdrive_file = 'gdrive_lookup_updated.json' #'gdrive_lookup.json'
 dropbox_file = 'dropbox_lookup.json'
+results_file = "results.json"
 overwrite = False
+
+
 
 
 def process_entry(data, d):
     entry = data[d]
     path = '%s/%s'%(image_dir, entry['name'])
     save_path = path.replace('gallery/', 'gallery/thumb/')
-
+    print("open", path)
     if entry['type'] == 'video':
         vid = cv2.VideoCapture(path)
         w, h = vid.get(cv2.CAP_PROP_FRAME_WIDTH), vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -74,6 +76,7 @@ def process_entry(data, d):
             os.system(cmd)     
 
     elif entry['type'] == 'photo':
+        return
         img = Image.open(path)
         w, h = img.width, img.height
         h2 = height_small if w > h else height_large
@@ -88,14 +91,17 @@ def process_entry(data, d):
 def lineup_gdrive_links():
     data = json.loads(open(data_path, 'rb').read())
     images = data['images']
-    lookup = json.loads(open(lookup_path, 'rb').read())
+    lookup = json.loads(open(gdrive_file, 'rb').read())
     gdrive_files = list(set(lookup.keys()))
     local_files = list(set([img['name'] for img in images]))
     images_non_gdrive = [f for f in local_files if f not in gdrive_files]
     duplicates = [item for item, count in collections.Counter([img['name'] for img in images]).items() if count > 1]
     for d in tqdm(range(len(images))):
-        images[d]['gdrive'] = lookup[images[d]['name']]
-    json_file = open("results.json", "w")
+        if images[d]['name'] in lookup:
+            images[d]['gdrive'] = lookup[images[d]['name']]
+        else:
+            print("ERROR! nothing found for %s"%images[d]['name'])
+    json_file = open(results_file, "w")
     data['images'] = images
     json_file.write(json.dumps(data, indent=4))
     json_file.close()
@@ -107,7 +113,7 @@ def make_thumbnails():
     for d in tqdm(range(len(images))):
         process_entry(images, d)
     data['images'] = images
-    json_file = open("results.json", "w")
+    json_file = open(results_file, "w")
     json_file.write(json.dumps(data, indent=4))
     json_file.close()
 
@@ -124,13 +130,35 @@ def lineup_dropbox_links():
         if name in file_names:
             img['dropbox'] = db[name]
     data['images'] = images
-    json_file = open("results.json", "w")
+    json_file = open(results_file, "w")
     json_file.write(json.dumps(data, indent=4))
     json_file.write(json.dumps(data, indent=4))
 
 
+def check():
+    data = json.loads(open(data_path, 'rb').read())
+    images = data['images']
+    names = [img['name'] for img in images]
+    duplicates = [item for item, count in collections.Counter([name for name in names]).items() if count > 1]
+    photos = [img for img in images if img['type']=='photo']
+    videos = [img for img in images if img['type']=='video']
+    print('duplicates', duplicates)
+    print("%d photos, %d videos" % (len(photos), len(videos)))
+    no_gdrive = [img for img in photos if 'gdrive' not in img]
+    no_dropbox = [img for img in videos if 'dropbox' not in img]
+    print('no gdrive: ', no_gdrive)
+    print('no dropbox: ', no_dropbox)
+    # duplicates
+    # any tags don't belong?
+    # any videos without dropbox?
+    # any images without gdrive?
+    pass
+
+
+
+make_thumbnails()
 #lineup_gdrive_links()
 #lineup_dropbox_links()
-make_thumbnails()
+# check()
 
 
